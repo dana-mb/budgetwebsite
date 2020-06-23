3a4172a37918f12d6da2e4ea253135726d2569ff
Dana Boyko <danamboyko@gmail.com>
2020-06-23 21:13:55 +0300

    change the config file again to write the port in the correct way

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
     //Database Connection Constants
 
-    define('DB_HOST','budget-website-db.cfmeaa0ngg85.eu-central-1.rds.amazonaws.com,3306'); //previous DB_HOST is 'localhost'
+    define('DB_HOST','budget-website-db.cfmeaa0ngg85.eu-central-1.rds.amazonaws.com:3306'); //previous DB_HOST is 'localhost'
     define('DB_USER','dana'); //previous 'budget'
     define('DB_PASSWORD','nhkehktc22'); //previous 'TwlkckBbAWIR'
     define('DB_NAME','Budget Website');
