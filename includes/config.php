f0b314b966aaa89c718755ce8690f5c385284954
Dana Boyko <danamboyko@gmail.com>
2020-06-23 20:55:04 +0300

    insert phpmyadmin + change the config file to work on aws

--- includes/config.php
+++ includes/config.php
@@ -2,9 +2,9 @@
 
     //Database Connection Constants
 
-    define('DB_HOST','localhost');
-    define('DB_USER','budget');
-    define('DB_PASSWORD','TwlkckBbAWIR');
+    define('DB_HOST','budget-website-db.cfmeaa0ngg85.eu-central-1.rds.amazonaws.com,3306'); //previous DB_HOST is 'localhost'
+    define('DB_USER','dana'); //previous 'budget'
+    define('DB_PASSWORD','nhkehktc22'); //previous 'TwlkckBbAWIR'
     define('DB_NAME','Budget Website');
 
     
