6c1c31640a2049a19c5f38662b2a9022eafda9a4
Dana Boyko <danamboyko@gmail.com>
2020-06-29 16:58:34 +0300

    make the website fit for localhost instead for amazon-2

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
     //Database Connection Constants
 
-    define('DB_HOST','budget-website-db.cfmeaa0ngg85.eu-central-1.rds.amazonaws.com:3306'); //on my localhost: 'localhost', on amazon: 'budget-website-db.cfmeaa0ngg85.eu-central-1.rds.amazonaws.com:3306'
+    define('DB_HOST','localhost'); //on my localhost: 'localhost', on amazon: 'budget-website-db.cfmeaa0ngg85.eu-central-1.rds.amazonaws.com:3306'
     define('DB_USER','budget'); //on my localhost: 'budget', on amazon: 'dana'
     define('DB_PASSWORD','TwlkckBbAWIR'); //on my localhost: 'TwlkckBbAWIR', on amazon: 'nhkehktc22'
     define('DB_NAME','Budget Website');
