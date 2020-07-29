a1d6250196a88cbe38d004da84bce3fb98af62cd
Dana Boyko <danamboyko@gmail.com>
2020-07-29 10:50:13 +0300

    reduce the amount of text & change it in the index page so that everything will fit the height of a phone screen fixes #28

--- index.php
+++ index.php
@@ -32,65 +32,51 @@ $session->session_and_cookie_check();
       <hr class="my-4">
       <h4 id="index_message"></h4>
       
-      <div class="container" id="signup-login-box" style="max-width: 550px">
-      
-        <form method="post" class="col-md-12" id="sign-up-form">
-        <h5>Sign Up</h5>
-          <br>
-          <div class="form-group row">
-            <label for="inputEmail" class="col-sm-2 col-form-label">Email:</label>
-            <div class="col-sm-10">
+      <div class="container" id="signup-login-box" style="max-width: 300px">
+
+        <ul class=" nav nav-pills justify-content-center">
+          <li><a class="nav-link active" data-toggle="pill" href="#log-in-form">Log In</a></li>
+          <li><a class="nav-link" data-toggle="pill" href="#sign-up-form">Sign Up</a></li>
+        </ul>
+
+        <div class="tab-content">
+          
+          <form method="post" class="tab-pane col-md-12" id="sign-up-form">
+            <br>
+            <div class="form-group row">
               <input type="email" class="form-control" name="sign-up-email" id="sign-up-inputEmail" placeholder="email@example.com">
             </div>
-          </div>
-          <div class="form-group row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Password:</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" name="sign-up-password" id="sign-up-inputPassword" placeholder="Password" autocomplete="on">
+            <div class="form-group row">
+              <input type="password" class="form-control" name="sign-up-password" id="sign-up-inputPassword" placeholder="password" autocomplete="on">
             </div>
-          </div>
-          <div class="form-group row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Password Verify:</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" name="sign-up-password-verify" id="sign-up-inputPassword-verify" placeholder="Password" autocomplete="on">
+            <div class="form-group row">
+              <input type="password" class="form-control" name="sign-up-password-verify" id="sign-up-inputPassword-verify" placeholder="password verification" autocomplete="on">
             </div>
-          </div>
-          <div class="form-group">
-            <button type="submit" form="sign-up-form" name="sign-up" id="sign-up" class="btn btn-outline-success">Sign Up</button>
-          </div>
-          <p><b>Signed up already?</b></p>
-          <input type="button" class="toggle-forms" id="showLogInForm" value="Log In Form"></input>
-        </form>
+            <div class="form-group">
+              <button type="submit" form="sign-up-form" name="sign-up" id="sign-up" class="btn btn-outline-success">Sign Up</button>
+            </div>
+          </form>
 
-        <form method="POST" class="col-md-12" id="log-in-form">
-        <h5>Log In</h5>
-          <br>
-          <div class="form-group row">
-            <label for="inputEmail" class="col-sm-2 col-form-label">Email:</label>
-            <div class="col-sm-10">
+          <form method="POST" class="tab-pane active col-md-12" id="log-in-form">
+            <br>
+            <div class="form-group row">
               <input type="email" class="form-control" name="log-in-email" id="log-in-inputEmail" placeholder="email@example.com">
             </div>
-          </div>
-          <div class="form-group row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Password:</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" name="log-in-password" id="log-in-inputPassword" placeholder="Password" autocomplete="on">
+            <div class="form-group row">
+              <input type="password" class="form-control" name="log-in-password" id="log-in-inputPassword" placeholder="password" autocomplete="on">
             </div>
-          </div>
-          <div class="form-group">
-            <div class="form-check">              
-              <input type="checkbox" id="visigned" class="form-check-input" name="stayLoggedIn" value="1">
-              <label class="form-check-label" for="visigned">stay logged in</label>
-            </div>        
-          </div>
-          <div class="form-group">
-            <button type="submit" form="log-in-form" name="log-in" id="log-in" class="btn btn-outline-success" >Log In</button>
-          </div>
-
-          <p><b>Not signed up yet?</b></p>
-          <input type="button" class="toggle-forms" id="showSignUpForm" value="Sign Up Form">
-        </form>
-
+            <div class="form-group">
+              <div class="form-check">              
+                <input type="checkbox" id="visigned" class="form-check-input" name="stayLoggedIn" value="1">
+                <label class="form-check-label" for="visigned">stay logged in</label>
+              </div>        
+            </div>
+            <div class="form-group">
+              <button type="submit" form="log-in-form" name="log-in" id="log-in" class="btn btn-outline-success" >Log In</button>
+            </div>
+          </form>
+          
+        </div>
       </div>
     </div>
     
