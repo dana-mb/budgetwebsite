3d0549f0b2ef4aefaed5efa89f9a6bfc4aaeb9a2
Dana Boyko <danamboyko@gmail.com>
2020-10-03 11:06:03 +0300

    move other websites to their own repo

--- citiesHoursCalculator/ajax/getTime.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $url = "http://worldtimeapi.org/api/timezone/".$_POST['location'];
-    $data = json_decode(file_get_contents($url), true);
-    $datetime = $data['datetime'];
-    $time = substr($datetime, strpos($datetime, "T")+1, -13);
-    echo $time;
-    
-?>
